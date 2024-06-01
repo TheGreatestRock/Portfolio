268ff14bf0e367b55fea617cfc017e215bb2f404
TheGreatestRock <72756140+TheGreatestRock@users.noreply.github.com>
2024-06-01 15:52:26 +0200

    first commit

---
+++ Portfolio/portfolio_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
